f49c2b46ac1d7fc4dea1aa42f303bcbd80f9e403
Jakub Krupiński <jakub.krupinski20.03@gmail.com>
2024-03-16 15:02:02 +0100

    Polaczylem z postgresem, dodalem liquibase, przyklad jak zrobic nowa tabele

---
+++ src/main/java/org/example/czy_dojade_backend/models/Employee.java
@@ -0,0 +1,20 @@
+package org.example.czy_dojade_backend.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+}
