07b5dd93ba1f7c546585754488ee72c7d3fe3577
Jakub Krupiński <jakub.krupinski20.03@gmail.com>
2024-03-16 13:49:24 +0100

    Repo init

---
+++ src/main/java/org/example/czy_dojade_backend/CzyDojadeBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.czy_dojade_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CzyDojadeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CzyDojadeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/czy_dojade_backend/CzyDojadeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.czy_dojade_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CzyDojadeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
