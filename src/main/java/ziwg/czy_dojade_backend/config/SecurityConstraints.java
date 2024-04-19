004117410665f2170f07826d96dfda3a0d3b727a
Jakub Krupiński <jakub.krupinski20.03@gmail.com>
2024-04-19 02:35:37 +0200

    oops

--- src/main/java/ziwg/czy_dojade_backend/config/SecurityConstraints.java
+++ src/main/java/ziwg/czy_dojade_backend/config/SecurityConstraints.java
@@ -1,8 +1,6 @@
 package ziwg.czy_dojade_backend.config;
 
 public class SecurityConstraints {
-    public static final String SECRET = "SECRET_KEY";
-    public static final long EXPIRATION_TIME = 900_00; // 15 mins
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String FRONTEND_URL = "http://localhost:3000";
